859d7366bf292799dd52c261e7f926af1a06b169
Alexander Sorokin <bst28@yandex.ru>
2017-06-09 18:58:56 +0300

    Добавил логгирование с использованием log4j2.

--- src/main/java/controller/ServiceController.java
+++ src/main/java/controller/ServiceController.java
@@ -4,18 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Controller
 public class ServiceController {
 
+    private static Logger LOG = LogManager.getLogger(ServiceController.class);
+
     @RequestMapping("/test")
     public String test(@RequestParam(value="text", defaultValue="default") String text) {
+        LOG.trace("text " + text);
         return "Hello! Your text: " + text;
     }
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        LOG.trace("name " + name);
         return "greeting";
     }
 
